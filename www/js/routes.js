eeb3092a52b810f7d84e41a5596f746abf7f269f
GlobalFoundries <gfmobile@GlobalFoundriess-MacBook-Pro.local>
2016-07-29 17:47:40 +0800

    New Template Added

---
+++ www/js/routes.js
@@ -0,0 +1,71 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('tabsController.posts', {
+    url: '/page2',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/posts.html',
+        controller: 'postsCtrl'
+      }
+    }
+  })
+
+  .state('cartTabDefaultPage', {
+    url: '/page3',
+    templateUrl: 'templates/cartTabDefaultPage.html',
+    controller: 'cartTabDefaultPageCtrl'
+  })
+
+  .state('tabsController.cloudTabDefaultPage', {
+    url: '/page4',
+    views: {
+      'tab3': {
+        templateUrl: 'templates/cloudTabDefaultPage.html',
+        controller: 'cloudTabDefaultPageCtrl'
+      }
+    }
+  })
+
+  .state('tabsController', {
+    url: '/page1',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+  .state('login', {
+    url: '/page5',
+    templateUrl: 'templates/login.html',
+    controller: 'loginCtrl'
+  })
+
+  .state('signup', {
+    url: '/page6',
+    templateUrl: 'templates/signup.html',
+    controller: 'signupCtrl'
+  })
+
+  .state('tabsController.postDetails', {
+    url: '/page7',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/postDetails.html',
+        controller: 'postDetailsCtrl'
+      }
+    }
+  })
+
+$urlRouterProvider.otherwise('/page5')
+
+  
+
+});
